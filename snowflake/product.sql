781966cf978eca99fd61dc62647cf380dd5c4126
Rahul Ravi <Rahul.Ravi@cognizant.com>
2025-08-06 14:20:01 +0000

    .

---
+++ snowflake/product.sql
@@ -0,0 +1,4 @@
+select PRODUCT_ID, SKU, PRODUCT_NAME , Case when Category = 'Category 1' then 'Electronics' when category = 'Category 2' then 'Electrical'
+else category end as CATEGORY
+from source_snowflake_db.data.product
+order by PRODUCT_ID
